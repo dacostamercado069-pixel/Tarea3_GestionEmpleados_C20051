c9134467b95b2911aebc5d3cb7f5e34f50900f64
Kristy Acosta <dacostamercado069@gmail.com>
2026-05-01 22:24:04 -0600

    Agregar archivos de proyecto.

---
+++ Tarea3_GestionEmpleados_[C20051]/Models/Empleado.cs
@@ -0,0 +1,7 @@
+﻿namespace Tarea3_GestionEmpleados__C20051_.Models {
+    using System.ComponentModel.DataAnnotations;
+
+    public class Empleado
+    {
+    }
+}
---
+++ Tarea3_GestionEmpleados_[C20051]/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tarea3_GestionEmpleados__C20051_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
