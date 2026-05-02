d627118849b979d8e823ce321c8425ac44216d20
Kristy Acosta <dacostamercado069@gmail.com>
2026-05-01 22:36:11 -0600

    Modelo Empleado con Data Annotations

--- Tarea3_GestionEmpleados_[C20051]/Models/Empleado.cs
+++ Tarea3_GestionEmpleados_[C20051]/Models/Empleado.cs
@@ -3,5 +3,27 @@
 
     public class Empleado
     {
+        public int Id { get; set;}
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set;}
+
+        [Required]
+        [StringLength(100)]
+        public string Apellido { get; set;}
+
+        [Required]
+        public string Departamento { get; set;}
+
+        [Required]
+        [Range(400000, 10000000)]
+        public decimal Salario { get; set;}
+
+        public DateTime FechaIngreso { get; set;}
+
+        public bool Activo { get; set;}
+
+        public string NombreCompleto => $"{Nombre} {Apellido}";
     }
 }
