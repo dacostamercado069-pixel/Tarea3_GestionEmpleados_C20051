b2a30cc9414dcad0529371ed68271015e1d0db8b
Kristy Acosta <dacostamercado069@gmail.com>
2026-05-01 22:55:07 -0600

    Configuración  DbContext  y conexión a base de datos

---
+++ Tarea3_GestionEmpleados_[C20051]/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Tarea3_GestionEmpleados__C20051_.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Tarea3_GestionEmpleados__C20051_.Models;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options) 
+        { }
+        public DbSet<Empleado> Empleados { get; set;}
+
+    }
+}
